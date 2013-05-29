318547acd421668a7ef46fc0676c3d4e77bec6ae
jkroso <jkroso@gmail.com>
2013-05-30 10:13:15 +1200

    rename dots -> templates

--- bin/install.sh
+++ bin/install.sh
@@ -8,8 +8,8 @@ if [[ -d $HOME"/.fsplates" ]]; then
 	printf "'~/.fsplates' already exists so no files written to it\n"
 	printf "if you want to update your templates you should do it manually\n"
 else
-	if [[ -d $DIR"/dots" ]]; then
-		cp -r $DIR"/dots" $HOME"/.fsplates"
+	if [[ -d $DIR"/templates" ]]; then
+		cp -r $DIR"/templates" $HOME"/.fsplates"
 		printf "done!\n"
 	else
 		printf "unable to find default templates. Try installing straight from github\n"
