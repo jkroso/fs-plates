e1727f46fc6420104fef47a36bc873a1d9e3d4f5
Jake Rosoman <jkroso@gmail.com>
2014-01-27 01:42:30 +1300

    update templates

---
+++ templates/node/test/hydro.conf.js
@@ -0,0 +1,24 @@
+
+/**
+ * Hydro configuration
+ *
+ * @param {Hydro} hydro
+ */
+
+module.exports = function(hydro) {
+  hydro.set({
+    suite: '{{project_name}}',
+    timeout: 500,
+    plugins: [
+      require('hydro-chai'),
+      require('hydro-bdd')
+    ],
+    chai: {
+      chai: require('chai'),
+      plugins: [ require('chai-spies') ],
+      styles: ['should', 'expect', 'assert'],
+      stack: true
+    },
+    globals: {}
+  })
+}
